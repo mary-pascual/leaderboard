623b750dddbfc454fdc5aa34d62e300956e39a29
Mary <mekp@hawaii.edu>
2017-02-28 13:57:00 -1000

    Task 10: Methods

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,21 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+@mary-pascual
+Sign out
+Watch 2
+Star 4
+Fork 6 realdavidturnbull/leaderboard-v4
+Code  Issues 3  Pull requests 0  Projects 0  Wiki  Pulse  Graphs
+Tree: 7b28d22ad0 Find file Copy pathleaderboard-v4/leaderboard.js
+7b28d22  on Apr 8, 2016
+dturnbull 12 - Methods
+0 contributors
+RawBlameHistory
+88 lines (83 sloc)  2.87 KB
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
@@ -26,27 +44,22 @@ if(Meteor.isClient){
     },
     'click .increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+      Meteor.call('updateScore', selectedPlayer, 5);
     },
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+      Meteor.call('updateScore', selectedPlayer, -5);
     },
     'click .remove': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove({ _id: selectedPlayer });
+      Meteor.call('removePlayer', selectedPlayer);
     }
   });
   Template.addPlayerForm.events({
     'submit form': function(){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score: 0,
-        createdBy: currentUserId
-      });
+      Meteor.call('createPlayer', playerNameVar);
       event.target.playerName.value = "";
     }
   });
@@ -61,3 +74,35 @@ if(Meteor.isServer){
     return PlayersList.find({ createdBy: currentUserId });
   });
 }
+
+Meteor.methods({
+  'createPlayer': function(playerNameVar){
+    check(playerNameVar, String);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    }
+  },
+  'removePlayer': function(selectedPlayer){
+    check(selectedPlayer, String);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.remove({ _id: selectedPlayer, createdBy: currentUserId });
+    }
+  },
+  'updateScore': function(selectedPlayer, scoreValue){
+    check(selectedPlayer, String);
+    check(scoreValue, Number);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.update( { _id: selectedPlayer, createdBy: currentUserId },
+          { $inc: {score: scoreValue} });
+    }
+  }
+});
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
